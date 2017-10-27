0821ff8619cef4a1e9147f4109ea161039c744e9
Gabriel Debais <gabriel.dbs.1@gmail.com>
2017-10-27 17:10:57 -0300

    avance de th3

--- src/Integrador.f90
+++ src/Integrador.f90
@@ -3,8 +3,31 @@ module integrador
 implicit none
 private
 
-	public :: TranformacionHa
-	public :: TranformacionHb
+	!public :: TranformacionHa
+!	public :: TranformacionHb
 	public :: TranformacionHc
-
+!vector  (q,y,x,p)  [12,N]
+!definir y hacer la matrizhc
 contains
+  subroutine TranformacionHc(vector,matrizhc)
+	real(16), dimension(:,:), intent(inout) :: vector
+  real(16), dimension(12) :: nvector
+  real(16), dimension(4,4), intent(in) :: matrizhc
+  integer :: i
+  integer :: k
+  integer :: l
+  nvector=0
+  do i=1,2
+    do l=1,3
+      do k=1,4
+        nvector(l)=nvector(l)+0.5*matrizhc(k,1)*vector(l+3*(k-1),i)
+        nvector(l+3)=nvector(l+3)+0.5*matrizhc(k,2)*vector(l+3*(k-1),i)
+        nvector(l+6)=nvector(l+6)+0.5*matrizhc(k,3)*vector(l+3*(k-1),i)
+        nvector(l+9)=nvector(l+9)+0.5*matrizhc(k,4)*vector(l+3*(k-1),i)
+      end do
+    end do
+    vector(:,i)=nvector
+  end do
+	end subroutine
+
+end module
