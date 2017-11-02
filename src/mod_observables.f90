903941c62a80fe110357be397b1cb2b18c0db40f
pugliese <facu_pugliese@hotmail.com>
2017-11-02 10:27:36 -0300

    Corregidos errores de compilacion en observables

--- src/mod_observables.f90
+++ src/mod_observables.f90
@@ -16,17 +16,20 @@ module observables
 			real(16), dimension(:,:), intent(in) :: vector
 			integer :: i
 			EnergiaCinetica = 0
-			do i=1,ubound(vector)
-				EnergiaCinetica = EnergiaCinetica + vector(i,10)*vector(i,10) + vector(i,11)*vector(i,11) + vector(i,12)*vector(i,12)  ! Medio pajero, pero deberia funcionar
+			do i=1,ubound(vector,1)
+				EnergiaCinetica = EnergiaCinetica + vector(i,10)*vector(i,10) + vector(i,11)*vector(i,11) + vector(i,12)*vector(i,12)
 			end do
 		end function
 
 		real(16) function EnergiaPotencial(vector,L,LUT,V)
 			real(16), dimension(:,:), intent(in) :: vector
+			real(16), dimension(:), intent(in) :: LUT
+			real(16), intent(in) :: L
+			real(16), intent(in) :: V
 			integer :: i
 			integer :: j
 			EnergiaPotencial = 0
-			do i=2,ubound(vector)
+			do i=2,ubound(vector,1)
 				do j=1,(i-1)
 					EnergiaPotencial = EnergiaPotencial + Valor_LUT(LUT,DistanciaCuad(vector,i,j,L,1)+DistanciaCuad(vector,i,j,L,4))
 				end do
